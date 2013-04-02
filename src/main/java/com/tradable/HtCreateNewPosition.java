86ba8c5a67930f5fa346f10eac7884f9e3015bdf
John-David Wuarin <john.dwuarin@gmail.com>
2013-04-02 11:27:55 +0200

    Added code example to place a market order

--- src/main/java/com/tradable/HtCreateNewPosition.java
+++ src/main/java/com/tradable/HtCreateNewPosition.java
@@ -2,19 +2,78 @@ package com.tradable;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tradable.api.component.WorkspaceModule;
 import com.tradable.api.component.state.PersistedStateHolder;
+import com.tradable.api.entities.Instrument;
+import com.tradable.api.entities.OrderDuration;
+import com.tradable.api.entities.OrderSide;
+import com.tradable.api.entities.OrderType;
+import com.tradable.api.services.executor.OrderAction;
+import com.tradable.api.services.executor.OrderActionRequest;
+import com.tradable.api.services.executor.PlaceOrderActionBuilder;
+import com.tradable.api.services.executor.TradingRequest;
+import com.tradable.api.services.executor.TradingRequestExecutor;
+import com.tradable.api.services.executor.TradingRequestListener;
+import com.tradable.api.services.executor.TradingResponse;
 
-public class HtCreateNewPosition extends JPanel implements WorkspaceModule{
+public class HtCreateNewPosition extends JPanel implements WorkspaceModule, TradingRequestListener{
 	private static final String TITLE = "Create a new Position";
+
+	private static final Logger logger = LoggerFactory.getLogger(HtCreateNewPosition.class);
+
+	private TradingRequestExecutor executor;
+
+	private int commandIdSeed;
+
+	@Autowired
+	public void setExecutor(TradingRequestExecutor executor) {
+		this.executor = executor;		
+	}	
 	
-	public HtCreateNewPosition() {
+	
+	
+	public HtCreateNewPosition(TradingRequestExecutor executor) {
 		
 		setLayout(null);
 		setSize(250, 250);
 		
 	}
+	
+	
+	  public void placeMarketOrder(Integer accountId, Instrument instrument, OrderSide orderSide, OrderDuration orderDuration, Double quantity) {
+	      PlaceOrderActionBuilder orderActionBuilder = new PlaceOrderActionBuilder();
+	      orderActionBuilder.setInstrument(instrument);
+	      orderActionBuilder.setOrderSide(orderSide);
+	      orderActionBuilder.setDuration(orderDuration);
+	      orderActionBuilder.setOrderType(OrderType.MARKET);
+	      orderActionBuilder.setQuantity(quantity);
+
+	      OrderAction orderAction = orderActionBuilder.build();
+
+	      OrderActionRequest request = new OrderActionRequest(++commandIdSeed, accountId, orderAction);
+
+	      logger.info("Executing command: {}", commandIdSeed);
+
+	      try {
+	          executor.execute(request, this);
+	      } catch (RuntimeException ex) {
+	          logger.error("Failed to submit command: {}", commandIdSeed, ex);
+	      }
+	  }
+
+	  @Override
+	  public void requestExecuted(TradingRequestExecutor executor, TradingRequest request, TradingResponse response) {
+	      if (response.isSuccess()) {
+	          logger.info("Command is successfully executed: {}", request.getId());
+	      } else {
+	          logger.error("Command is failed to execute: {}", request.getId(), response.getCause());
+	      }
+	  }
+	 
 
 
 
--- src/main/java/com/tradable/HtCreateNewPositionFactory.java
+++ src/main/java/com/tradable/HtCreateNewPositionFactory.java
@@ -1,11 +1,17 @@
 package com.tradable;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.tradable.api.component.WorkspaceModule;
 import com.tradable.api.component.WorkspaceModuleCategory;
 import com.tradable.api.component.WorkspaceModuleFactory;
+import com.tradable.api.services.executor.TradingRequestExecutor;
 
 public class HtCreateNewPositionFactory implements WorkspaceModuleFactory{
 
+	@Autowired
+	private TradingRequestExecutor executor;  //create a trading request executor in 
+	
 	/**
 	 * @param args
 	 */
@@ -17,7 +23,7 @@ public class HtCreateNewPositionFactory implements WorkspaceModuleFactory{
 	@Override
 	public WorkspaceModule createModule() {
 		// TODO Auto-generated method stub
-		return null;
+		return new HtCreateNewPosition(executor);
 	}
 
 	@Override
@@ -39,3 +45,69 @@ public class HtCreateNewPositionFactory implements WorkspaceModuleFactory{
 	}
 
 }
+
+
+
+/*
+ * package com.tradable.api.samples;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tradable.api.entities.Instrument;
+import com.tradable.api.entities.OrderDuration;
+import com.tradable.api.entities.OrderSide;
+import com.tradable.api.entities.OrderType;
+import com.tradable.api.services.executor.OrderAction;
+import com.tradable.api.services.executor.OrderActionRequest;
+import com.tradable.api.services.executor.PlaceOrderActionBuilder;
+import com.tradable.api.services.executor.TradingRequest;
+import com.tradable.api.services.executor.TradingRequestExecutor;
+import com.tradable.api.services.executor.TradingRequestListener;
+import com.tradable.api.services.executor.TradingResponse;
+
+ public class OrderActionRequestSample implements TradingRequestListener {
+
+  private static final Logger logger = LoggerFactory.getLogger(OrderActionRequestSample.class);
+
+  private TradingRequestExecutor executor;
+
+  private int commandIdSeed;
+
+  @Autowired
+  public void setExecutor(TradingRequestExecutor executor) {
+      this.executor = executor;
+  }
+
+  public void placeMarketOrder(Integer accountId, Instrument instrument, OrderSide orderSide, OrderDuration orderDuration, Double quantity) {
+      PlaceOrderActionBuilder orderActionBuilder = new PlaceOrderActionBuilder();
+      orderActionBuilder.setInstrument(instrument);
+      orderActionBuilder.setOrderSide(orderSide);
+      orderActionBuilder.setDuration(orderDuration);
+      orderActionBuilder.setOrderType(OrderType.MARKET);
+      orderActionBuilder.setQuantity(quantity);
+
+      OrderAction orderAction = orderActionBuilder.build();
+
+      OrderActionRequest request = new OrderActionRequest(++commandIdSeed, accountId, orderAction);
+
+      logger.info("Executing command: {}", commandIdSeed);
+
+      try {
+          executor.execute(request, this);
+      } catch (RuntimeException ex) {
+          logger.error("Failed to submit command: {}", commandIdSeed, ex);
+      }
+  }
+
+  @Override
+  public void requestExecuted(TradingRequestExecutor executor, TradingRequest request, TradingResponse response) {
+      if (response.isSuccess()) {
+          logger.info("Command is successfully executed: {}", request.getId());
+      } else {
+          logger.error("Command is failed to execute: {}", request.getId(), response.getCause());
+      }
+  }
+ }
+ */
