3a52cdf1db59376f3765a5754081868c7b3dc44e
John-David Wuarin <john.dwuarin@gmail.com>
2013-04-12 01:15:08 +0200

    extracted code to place order. Put it in own class.

--- src/main/java/com/tradable/exampleApps/createPosition/HtCreateNewPosition.java
+++ src/main/java/com/tradable/exampleApps/createPosition/HtCreateNewPosition.java
@@ -104,23 +104,14 @@ import com.tradable.api.entities.OrderDuration;
 import com.tradable.api.entities.OrderSide;
 import com.tradable.api.entities.OrderStatus;
 import com.tradable.api.entities.OrderType;
-import com.tradable.api.services.executor.ModifyOrderAction;
-import com.tradable.api.services.executor.ModifyOrderActionBuilder;
-import com.tradable.api.services.executor.OrderAction;
-import com.tradable.api.services.executor.OrderActionRequest;
-import com.tradable.api.services.executor.PlaceOrderAction;
-import com.tradable.api.services.executor.PlaceOrderActionBuilder;
-import com.tradable.api.services.executor.TradingRequest;
 import com.tradable.api.services.executor.TradingRequestExecutor;
-import com.tradable.api.services.executor.TradingRequestListener;
-import com.tradable.api.services.executor.TradingResponse;
-import com.tradable.api.services.executor.groups.CreateOCOGroupRequestBuilder;
-import com.tradable.api.services.executor.groups.OrderGroupRequest;
+
+
 //====================================================================================
 //====================================================================================
 
 public class HtCreateNewPosition extends JPanel implements WorkspaceModule, 
-		TradingRequestListener, CurrentAccountServiceListener, InstrumentServiceListener, 
+		CurrentAccountServiceListener, InstrumentServiceListener, 
 		QuoteTickListener, ActionListener{
 	
 	//====================================================================================
@@ -165,22 +156,12 @@ public class HtCreateNewPosition extends JPanel implements WorkspaceModule,
 	private Instrument instrument;
 	private QuoteTickService quoteTickService;
 	private QuoteTickSubscription subscription;
-	private Quote bid;
-	private Quote ask;
+	public static Quote bid;
+	public static Quote ask;
 	//==================================================================================	
 	//==================================================================================	
 	
-	//========================================(3)========================================//
-	//The TradingRequestExecutor object is in charge of executing the trades once all the 
-	//settings pertaining to it have been set. The commandIdSeed is just a number that allows
-	//us to track internally the number of the command we are at. This is usde in the log.
-	//==================================================================================
-	private TradingRequestExecutor executor;
-	private int commandIdSeed;
-	//==================================================================================	
-	//==================================================================================
-	
-	
+	private PlaceOrderClass placeOrderObject;
 	
 	private JTextPane textPane;
 	private JButton btnNewButton;
@@ -257,10 +238,11 @@ public class HtCreateNewPosition extends JPanel implements WorkspaceModule,
 		subscription = quoteTickService.createSubscription();
         subscription.setListener(this);  //num: subscription now listens to this (as I am implementing the quoteTickListener)
 		
-		//========================================(3)========================================//
-		this.executor = executor; //no need to add listeners as this object takes action.
-		
-		
+        //========================================(3)========================================//
+
+		placeOrderObject = new PlaceOrderClass(executor, logger);
+		placeOrderObject.setAccountId(accountId);
+        
 		clickRound = 0;
 		
 	}
@@ -286,7 +268,7 @@ public class HtCreateNewPosition extends JPanel implements WorkspaceModule,
 		currentAccount = accountSubscriptionService.getCurrentAccount();
         if (event.isReset()) {
     		accountId = currentAccount.getAccountId();
-    		
+
         } 
         else {
         	
@@ -575,13 +557,13 @@ public class HtCreateNewPosition extends JPanel implements WorkspaceModule,
 		
 		else if (clickRound == 1){
 			
-			placeOrder(instrument, OrderSide.SELL, OrderDuration.DAY, OrderType.MARKET, 2000.0); 
+			placeOrderObject.placeOrder(instrument, OrderSide.SELL, OrderDuration.DAY, OrderType.MARKET, 2000.0); 
 
 		}
 		
 		else if (clickRound == 2){
 			//setting the limit price at a value that will not be filled (15 % below the current asking price)
-			placeOrder(instrument, OrderSide.BUY, OrderDuration.DAY, OrderType.LIMIT, 1000.0, 0.85*ask.getPrice()); 
+			placeOrderObject.placeOrder(instrument, OrderSide.BUY, OrderDuration.DAY, OrderType.LIMIT, 1000.0, 0.85*ask.getPrice()); 
 			
 		}
 		
@@ -601,7 +583,7 @@ public class HtCreateNewPosition extends JPanel implements WorkspaceModule,
     			}
     		}
 
-			modifyOrder(orderToModify, OrderDuration.DAY, 5000.0);
+    		placeOrderObject.modifyOrder(orderToModify, OrderDuration.DAY, 5000.0);
 			try {
 				textPane.getDocument().insertString(textPane.getCaretPosition() , 
 						"Order is being modified \n\n" , null);
@@ -614,7 +596,21 @@ public class HtCreateNewPosition extends JPanel implements WorkspaceModule,
 		}
 		
 		else{
-			OCOOrder();
+			
+			//find a position you want to set an OCO order to.
+			//This code assumes a position long 3000 EURUSD was taken
+			//the current account. The position object is found that way
+			Position oCOPosition = null;
+			List<Position> positions = currentAccount.getPositions();
+			for (Position pos : positions){
+				if (pos.getInstrument().getSymbol().equals(instrument.getSymbol())
+						&& (pos.getQuantity() == 3000.0)){
+					oCOPosition = pos;
+				}
+				
+			}
+			
+			placeOrderObject.OCOOrder(oCOPosition, 0.98 * bid.getPrice(), 1.02 * ask.getPrice());
 			clickRound = 0;
 			return;
 					
@@ -623,132 +619,6 @@ public class HtCreateNewPosition extends JPanel implements WorkspaceModule,
 		++clickRound;
 	}
 	
-	
-	public void placeOrder(Instrument instrument, OrderSide orderSide, OrderDuration orderDuration, 
-			OrderType orderType, Double quantity){
-		placeOrder(instrument, orderSide, orderDuration, orderType, quantity, 0.0);
-	}
-	
-	public void placeOrder(Instrument instrument, OrderSide orderSide, OrderDuration orderDuration, 
-			OrderType orderType, Double quantity, Double limit) {
-		
-		PlaceOrderActionBuilder orderActionBuilder = new PlaceOrderActionBuilder();
-		orderActionBuilder.setInstrument(instrument); // instrument object set in constructor
-		orderActionBuilder.setOrderSide(orderSide);
-		orderActionBuilder.setDuration(orderDuration);
-		orderActionBuilder.setOrderType(orderType); //so as to have it work or pend for a while
-		if(orderType == OrderType.LIMIT && limit > 0.0)
-			orderActionBuilder.setLimitPrice(limit);
-		else if(orderType == OrderType.LIMIT && limit == 0.0) //missing limit price. 
-			return; //do nothing
-		else if (orderType == OrderType.MARKET && limit != 0.0) //setting limit to market order
-			return; //do nothing
-		else{}
-		
-
-		orderActionBuilder.setQuantity(quantity);
-		PlaceOrderAction orderAction = orderActionBuilder.build();
-		
-		OrderActionRequest request = new OrderActionRequest(accountId, orderAction); 
-		
-
-		logger.info("Executing command: {}", ++commandIdSeed);
-
-		try {
-			executor.execute(request, this);
-		} 
-		
-		catch (Exception ex) {
-			logger.error("Failed to submit command: {}", commandIdSeed, ex);
-		}	
-	}	
-	
-	
-	//==================================================================================//
-	//Beware when using the ModifyOrderAction classes, as they main contain bugs and thus 
-	//their behavior can be somewhat unexpected although it will seem to work most of the
-	//time when the market is open.
-	//==================================================================================//
-	public void modifyOrder(Order orderToModify, OrderDuration orderDuration, Double quantity){
-		
-		
-		ModifyOrderActionBuilder orderActionBuilder = new ModifyOrderActionBuilder(orderToModify);
-		orderActionBuilder.setOrderType(OrderType.LIMIT);
-		orderActionBuilder.setLimitPrice(1.01* ask.getPrice()); //setting ask price to get filled almost surely
-	    orderActionBuilder.setDuration(orderDuration);
-	    orderActionBuilder.setQuantity(quantity);
-
-	    ModifyOrderAction modOrderAction = orderActionBuilder.build();
-
-	    OrderActionRequest modRequest = new OrderActionRequest(accountId, modOrderAction);
-		
-	    logger.info("Executing command: {}", ++commandIdSeed);
-
-	    try {
-	    	executor.execute(modRequest, this);
-	    } 
-	    catch (RuntimeException  ex) {
-	    	logger.error("Failed to submit command: {}", commandIdSeed, ex);
-	    }
-		
-	}
-	
-	
-	public void OCOOrder(){
-			
-		CreateOCOGroupRequestBuilder OCOBuilder = new CreateOCOGroupRequestBuilder();
-
-		//find a position you want to set an OCO order to.
-		//This code assumes a position long 3000 EURUSD was taken
-		//the current account. The position object is found that way
-		Position oCOPosition = null;
-		List<Position> positions = currentAccount.getPositions();
-		for (Position pos : positions){
-			if (pos.getInstrument().getSymbol().equals(instrument.getSymbol())
-					&& (pos.getQuantity() == 3000.0)){
-				oCOPosition = pos;
-			}
-			
-		}
-		if (oCOPosition == null) {return;}
-		
-		//We then have to set the position to the builder.
-		//Quantity is set to 3000 too. Could be different.
-		//setStopLoss sets the price we want to set our stop loss to.
-		//This sets the price of the STOP order. We also specify here 
-		//that this is a SELL order (as we are long 3000 EURUSD)
-		//setTakeProfit sets the price for the LIMIT order. Our
-		//limit order is set slightly higher than the current price so 
-		//that we would actually take a profit is the price was hit.
-		OCOBuilder.setPosition(oCOPosition);
-		OCOBuilder.setQuantity(3000.0);
-		OCOBuilder.setRequestId(++commandIdSeed);
-		OCOBuilder.setStopLoss(0.98 * bid.getPrice(), OrderSide.SELL);
-		OCOBuilder.setTakeProfit(1.02 * ask.getPrice());
-
-		OrderGroupRequest orderGroupRequest = OCOBuilder.build();
-		
-		
-	    logger.info("Executing command: {}", commandIdSeed);
-
-	    try {
-	    	executor.execute(orderGroupRequest, this);
-	    } 
-	    catch (RuntimeException  ex) {
-	    	logger.error("Failed to submit command: {}", commandIdSeed, ex);
-	    }
-		
-	}
-	
-	@Override
-	public void requestExecuted(TradingRequestExecutor executor, TradingRequest request, TradingResponse response) {
-		if (response.isSuccess())
-			logger.info("Command is successfully executed: {}", request.getId());
-					
-		else 
-			logger.error("Command is failed to execute: {}", request.getId(), response.getCause());
-		
-	}
 	
 	
 	public List<Order> getWorkingOrdersList(){
@@ -793,7 +663,6 @@ public class HtCreateNewPosition extends JPanel implements WorkspaceModule,
 
 	@Override
 	public JComponent getVisualComponent() {
-		// TODO Auto-generated method stub
 		return this;
 	}
 
